9099cd2899c11bc61f5aa47c2ad96208f0b79535
kankan <2728360489@qq.com>
2022-06-28 20:56:15 +0800

    5.React实现Slot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-
+import NavBar from './NavBar'
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <NavBar>
+            <strong>bbb</strong>
+            <strong>aaa</strong>
+            <strong>ccc</strong>
+        </NavBar>
+        <NavBar leftSlot={<strong>bbb</strong>}
+                centerSlot={<strong>ccc</strong>}
+                rightSlot={<a href='/#'>ccc</a>}
+        ></NavBar>
+      </div>
     )
   }
 }
