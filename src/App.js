818a584743e3940c0f6a4658e3ca8fe514aa7214
kankan <2728360489@qq.com>
2022-07-05 20:05:18 +0800

    14.classnames的简单使用

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import classNames from 'classnames'
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      isActive:true
+    }
+  }
   render() {
+    const {isActive} = this.state
+    const isBar = false;
+    const errClass = "error";
+    const warnClass = undefined;
+
     return (
-      <div>App</div>
+      <div>
+        {/* 原生React中添加class方法 */}
+        <h2 className={'foo bar active title'}>我是标题1</h2>
+        <h2 className={'title '+(isActive?"active":"")}>我是标题2</h2>
+        <h2 className={['title',(isActive?"active":"")].join(" ")}>我是标题3</h2>
+        {/* classnames库添加class */}
+        <h2 className='foo bar active title'>我是标题4</h2>
+        <h2 className={classNames("foo","bar","active","title")}>我是标题5</h2>
+        <h2 className={classNames({"active":isActive,'bar':isBar},'title')}>我是标题6</h2>
+        <h2 className={classNames('foo',errClass,warnClass,{"active":isActive})}>我是标题7</h2>
+        <h2 className={classNames(['active','title'])}>我是标题8</h2>
+        <h2 className={classNames(["active",'"title',{"bar":isBar}])}>我是标题9</h2>
+      </div>
     )
   }
 }
